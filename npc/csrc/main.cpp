3d5fbada5b2b7f6dcef0195a52c346da64fa04c4
tracer-ysyx <tracer@ysyx.org>
2026-03-13 16:16:21 +0800

    > sim RTL 2023k8009929022 刘遥青 Linux liu-virtual-machine 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:16:21 up  8:10,  1 user,  load average: 0.36, 0.30, 0.19

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,6 +1,35 @@
-#include <stdio.h>
+// DESCRIPTION: Verilator: Verilog example module
+//
+// This file ONLY is placed under the Creative Commons Public Domain.
+// SPDX-FileCopyrightText: 2017 Wilson Snyder
+// SPDX-License-Identifier: CC0-1.0
+//======================================================================
+
+// Include common routines
+// Include model header, generated from Verilating "top.v"
+#include "Vtop.h"
+#include <nvboard.h>
+static TOP_NAME dut;
+void nvboard_bind_all_pins(TOP_NAME* top);
 
 int main() {
-  printf("Hello, ysyx!\n");
-  return 0;
+    int time = 0;
+
+    nvboard_bind_all_pins(&dut);
+    nvboard_init();
+    // Simulate until $finish
+
+    while (time <= 100000) {
+        nvboard_update();
+        if(dut.a == 1 && dut.b == 1){
+            time++;
+        }
+        dut.eval();
+        
+    }
+
+
+
+    // Return good completion status
+    return 0;
 }
