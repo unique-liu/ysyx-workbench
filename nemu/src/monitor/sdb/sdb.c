c6ac04d062e6d0920342d673e994497c83e36f53
tracer-ysyx <tracer@ysyx.org>
2026-03-15 17:03:20 +0800

    > compile NEMU 2023k8009929022 刘遥青 Linux liu-virtual-machine 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:03:20 up  3:24,  0 users,  load average: 0.27, 0.14, 0.13

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -141,14 +141,21 @@ static int cmd_x(char *args){
     return 0;
   }
 
-  //translate address to print
+  //translate address to print and check if it is valid
   char *endptr = NULL;
   vaddr_t addr = strtoul(arg, &endptr, 16);
   if (*endptr != '\0') {
     printf("Invalid address: %s\n", arg);
     return 0;
   }
-  printf("----- check mem: 0x%08x ~ 0x%08x -----\n", (unsigned int)addr, (unsigned int)(addr + N * sizeof(word_t)));
+  vaddr_t end_addr = addr + N * sizeof(word_t);
+  if (addr >= 0x87ffffff || addr < 0x80000000 || end_addr > 0x87ffffff || end_addr < 0x80000000) {
+    printf("Invalid memory address: 0x%08x ~ 0x%08x\n", (unsigned int)addr, (unsigned int)end_addr);
+    return 0;
+  }
+
+  //print memory content
+  printf("----- check mem: 0x%08x ~ 0x%08x -----\n", (unsigned int)addr, (unsigned int)end_addr);
   for(int i = 0; i < N; i++){
     word_t data = vaddr_read(addr + i * sizeof(word_t), sizeof(word_t));
     printf("0x%08x: 0x%08x\n", (unsigned int)(addr + i * sizeof(word_t)), data);
