a2e41749fba065829fc4e27a758be2cebc649b30
tracer-ysyx <tracer@ysyx.org>
2026-03-16 20:31:34 +0800

    > compile NEMU 2023k8009929022 刘遥青 Linux liu-virtual-machine 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:31:34 up  1:10,  0 users,  load average: 0.47, 0.28, 0.22

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -370,7 +370,7 @@ word_t expr(char *e, bool *success) {
 
   /* TODO: Insert codes to evaluate the expression. */
   for (int i = 0; i < nr_token; i ++) {
-    if (tokens[i].type == '*' && (i == 0 || !(tokens[i - 1].type == TK_DECIMAL || tokens[i - 1].type == TK_HEX || tokens[i - 1].type == TK_REG) ) ) {
+    if (tokens[i].type == '*' && (i == 0 || !(tokens[i-1].type==TK_DECIMAL||tokens[i-1].type==TK_HEX||tokens[i-1].type==TK_REG||tokens[i-1].type==')') ) ) {
       tokens[i].type = TK_DEREF;
     }
   }
