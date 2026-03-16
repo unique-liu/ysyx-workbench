71bfdf842db7b7f078581b1de8bf5e6844253af8
tracer-ysyx <tracer@ysyx.org>
2026-03-16 14:55:11 +0800

    > compile NEMU 2023k8009929022 刘遥青 Linux liu-virtual-machine 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:55:11 up  6:47,  0 users,  load average: 0.40, 0.20, 0.19

--- nemu/include/debug.h
+++ nemu/include/debug.h
@@ -40,7 +40,21 @@
 
 #define TODO() panic("please implement me")
 
+#define USE_FLOG
+#ifdef USE_FLOG
 #define Flog(name,format, ...)\
     log_write(ANSI_FMT("[%s:%d %s][%s] " format,ANSI_FG_BLUE) "\n",__FILE__, __LINE__, __func__, name, ## __VA_ARGS__)
+#else
+#define Flog(name,format, ...)
+#endif
+
+#define USE_SDB_Flog
+#ifdef USE_SDB_Flog
+#define SDB_Flog Flog
+#else
+#define SDB_Flog(name,format, ...)
+#endif
+
+
 
 #endif
