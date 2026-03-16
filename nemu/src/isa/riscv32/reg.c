23d6631b229fd147d19eb17cbbb0f8787d137283
tracer-ysyx <tracer@ysyx.org>
2026-03-16 20:18:09 +0800

    > compile NEMU 2023k8009929022 刘遥青 Linux liu-virtual-machine 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:18:09 up 56 min,  0 users,  load average: 0.02, 0.20, 0.25

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -34,7 +34,11 @@ void isa_reg_display() {
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {
-  for(int i = 0; i < 32; i++){
+  if (strcmp(s, "0") == 0) {
+    *success = true;
+    return 0;
+  }
+  for(int i = 1; i < 32; i++){
     if(strcmp(s, regs[i]) == 0){
       *success = true;
       return cpu.gpr[i];
