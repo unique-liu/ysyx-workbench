1e8dfa8d72187b589dc3e8fed9977e3537d0db09
tracer-ysyx <tracer@ysyx.org>
2026-03-16 21:55:04 +0800

    > compile NEMU 2023k8009929022 刘遥青 Linux liu-virtual-machine 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:55:04 up  2:33,  0 users,  load average: 0.04, 0.08, 0.15

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -39,11 +39,13 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
 
+  #ifdef CONFIG_WATCHPOINT
   int trigger_wp = check_wp();
   if (trigger_wp!=0) {
     nemu_state.state = NEMU_STOP;
     printf("Hit %d watchpoints.\n", trigger_wp);
   }
+  #endif
 }
 
 static void exec_once(Decode *s, vaddr_t pc) {
