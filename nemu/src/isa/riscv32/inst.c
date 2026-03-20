40c55643a7aedc75e58d24c3ec22167fc13ff695
tracer-ysyx <tracer@ysyx.org>
2026-03-20 19:36:44 +0800

    > compile NEMU 2023k8009929022 刘遥青 Linux liu-virtual-machine 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:36:44 up  5:28,  1 user,  load average: 0.51, 0.27, 0.12

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -51,7 +51,9 @@ static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_
     case TYPE_N: break;
     default: panic("unsupported type = %d", type);
   }
-    printf("src1(r%02d)=0x%08x  src2(r%02d)= 0x%08x  rd(r%02d)  imm=\t0x%08x\n",rs1,*src1,rs2,*src2,*rd,*imm);
+  //debug log
+  // printf("src1(r%02d)=0x%08x  src2(r%02d)= 0x%08x  rd(r%02d)  imm=\t0x%08x\n",rs1,*src1,rs2,*src2,*rd,*imm);
+  //debug log end
 }
 
 static int decode_exec(Decode *s) {
