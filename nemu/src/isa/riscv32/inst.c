2570d062742f18014b0941629d1bbbfd5a726c1d
tracer-ysyx <tracer@ysyx.org>
2026-03-20 20:31:11 +0800

    > compile NEMU 2023k8009929022 刘遥青 Linux liu-virtual-machine 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:31:11 up  6:22,  1 user,  load average: 0.52, 0.35, 0.21

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -128,7 +128,7 @@ static int decode_exec(Decode *s) {
   //INSTPAT("0000001 ????? ????? 011 ????? 01100 11", mulhu  , R, R(rd) = (word_t)(((int64_t)src1 * (int64_t)src2) >> 32));
   INSTPAT("0000001 ????? ????? 100 ????? 01100 11", div    , R, R(rd) = (src1 == 0x80000000 && (int32_t)src2 == -1) ? 0x80000000 : (src2 != 0) ? (int32_t)src1 / (int32_t)src2 : 0xFFFFFFFF);
   //INSTPAT("0000001 ????? ????? 101 ????? 01100 11", divu   , R, R(rd) = (src2 != 0) ? src1 / src2 : 0xFFFFFFFF);
-  //INSTPAT("0000001 ????? ????? 110 ????? 01100 11", rem    , R, R(rd) = (src1 == 0x80000000 && (int32_t)src2 == -1) ? 0 : (src2 != 0) ? (int32_t)src1 % (int32_t)src2 : src1);
+  INSTPAT("0000001 ????? ????? 110 ????? 01100 11", rem    , R, R(rd) = (src1 == 0x80000000 && (int32_t)src2 == -1) ? 0 : (src2 != 0) ? (int32_t)src1 % (int32_t)src2 : src1);
   //INSTPAT("0000001 ????? ????? 111 ????? 01100 11", remu   , R, R(rd) = (src2 != 0) ? src1 % src2 : src1);
   //----- RV32M Standard Extension end
 
