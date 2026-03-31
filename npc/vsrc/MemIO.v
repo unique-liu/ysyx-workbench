3c2bc23c744bff1cda541219fb24f6cda24e920b
tracer-ysyx <tracer@ysyx.org>
2026-03-31 15:39:47 +0800

    > sim RTL 2023k8009929022 刘遥青 Linux liu-virtual-machine 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:39:47 up  1:51,  1 user,  load average: 0.54, 0.41, 0.46

--- npc/vsrc/MemIO.v
+++ npc/vsrc/MemIO.v
@@ -8,15 +8,18 @@ module MemIO (
     input  [3:0]  io_wmask
 );
     // DPI-C 导入声明与之前相同
-    import "DPI-C" function void mem_read(input int addr, output int data);
+    import "DPI-C" function int mem_read(input int addr);
     import "DPI-C" function void mem_write(input int addr, input int data, input byte mask);
 
     reg [31:0] rdata_reg;
     assign io_rdata = rdata_reg;
 
     always @(*) begin
-        if (io_ren) mem_read(io_raddr, rdata_reg);
-        else rdata_reg = 32'h0;
+        if (io_ren) begin
+            rdata_reg = mem_read(io_raddr);
+        end else begin
+            rdata_reg = 32'h0;
+        end
     end
 
     always @(*) begin
