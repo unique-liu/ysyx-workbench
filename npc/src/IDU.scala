abefa9f345797bc2a0694ce7050b0a00d2442892
tracer-ysyx <tracer@ysyx.org>
2026-03-31 18:23:48 +0800

    > generate verilog 2023k8009929022 刘遥青 Linux liu-virtual-machine 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:23:48 up  1:34,  0 users,  load average: 1.49, 2.01, 1.75

--- npc/src/IDU.scala
+++ npc/src/IDU.scala
@@ -157,7 +157,14 @@ class inst_decoder extends Module{
     // }
 
     def concatBitPat(patterns: BitPat*): BitPat = {
-        if (patterns.isEmpty) BitPat("b") else patterns.reduce(_ + _)
+        // 1. 拼接出完整的二进制字符串
+        val fullBitString = patterns.map(_.toString.stripPrefix("b")).mkString
+        
+        // 2. 【打印功能】在终端输出最终结果！
+        println("[concatBitPat 最终拼接位串] => b" + fullBitString)
+        
+        // 3. 生成最终BitPat
+        BitPat("b" + fullBitString)
     }
     val table = TruthTable(
         Map(
