6f0f5fdd1b13ea15f83d4dafa42de2c2a0257255
tracer-ysyx <tracer@ysyx.org>
2026-04-04 10:51:00 +0800

    > compile NEMU 2023k8009929022 刘遥青 Linux liu-virtual-machine 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:51:00 up  2:31,  0 users,  load average: 1.29, 0.67, 0.56

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -140,6 +140,10 @@ static void ftrace_call(word_t pc, word_t target,int rd) {
   
   int current_idx = find_function_by_addr(pc);
   int target_idx = find_function_by_addr(target);
+  if (current_idx == target && rd == 0) {
+    // should be "j" instruction, not a call, ignore it
+    return;
+  }
   log_write("[ftrace]:deep%2d [%10s@"FMT_PADDR"]call deep%2d[%10s@"FMT_PADDR"]\n", (int)ftrace_call_depth, ((current_idx != -1) ? func_table[current_idx].name : "???"), pc, (int)ftrace_call_depth+1, ((target_idx != -1) ? func_table[target_idx].name : "???"), target);
   if (ftrace_call_depth < FTRACE_MAX_CALL_DEPTH) {
     ftrace_call_stack[ftrace_call_depth] = (rd != 0)? pc+4 : 0;
@@ -166,10 +170,7 @@ static void ftrace_ret(word_t pc, word_t target) {
 void ftrace_enter(word_t pc, word_t target,int rd,int rs1){
   #ifdef CONFIG_FTRACE
   if (rs1 != 1) {
-    if (rd != 0) {
-      ftrace_call(pc, target,rd);
-    }
-    
+    ftrace_call(pc, target,rd);
   }else {
     ftrace_ret(pc, target);
   }
