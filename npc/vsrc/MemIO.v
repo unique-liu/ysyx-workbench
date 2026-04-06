c808440adc0617b9bdcc48a04487c5f9deda6bcf
tracer-ysyx <tracer@ysyx.org>
2026-04-06 10:51:02 +0800

    > sim RTL 2023k8009929022 刘遥青 Linux liu-virtual-machine 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:51:02 up  1:53,  0 users,  load average: 2.60, 2.72, 1.99

--- npc/vsrc/MemIO.v
+++ npc/vsrc/MemIO.v
@@ -1,13 +1,13 @@
 module MemIO (
     input         io_clock,
-    input         io_PC,
+    input  [31:0] io_PC,
     input         io_ren,
     input  [31:0] io_raddr,
     output [31:0] io_rdata,
     input         io_wen,
     input  [31:0] io_waddr,
     input  [31:0] io_wdata,
-    input  [3:0]  io_wmask
+    input  [3:0 ] io_wmask
 );
     // DPI-C 导入声明与之前相同
     import "DPI-C" function int mem_read(input int addr,input int pc);
