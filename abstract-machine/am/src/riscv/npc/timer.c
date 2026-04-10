7eb15e17fb1cc3750d2415242a9986e8b7735af5
tracer-ysyx <tracer@ysyx.org>
2026-04-10 14:19:04 +0800

    > sim RTL 2023k8009929022 刘遥青 Linux liu-virtual-machine 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:19:04 up 7 min,  0 users,  load average: 4.95, 3.23, 1.55

--- abstract-machine/am/src/riscv/npc/timer.c
+++ abstract-machine/am/src/riscv/npc/timer.c
@@ -1,10 +1,11 @@
 #include <am.h>
-
+#include "../riscv.h"
+#define RTC_ADDR 0xa0000048
 void __am_timer_init() {
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uptime->us = 0;
+  uptime->us = inl(RTC_ADDR) | ((uint64_t)inl(RTC_ADDR + 4) << 32);
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
--- abstract-machine/am/src/riscv/npc/trm.c
+++ abstract-machine/am/src/riscv/npc/trm.c
@@ -1,6 +1,7 @@
 #include <am.h>
 #include <klib-macros.h>
 #include "../riscv.h"
+#define SERIAL_ADDR 0x10000000
 
 extern char _heap_start;
 int main(const char *args);
@@ -13,7 +14,7 @@ Area heap = RANGE(&_heap_start, PMEM_END);
 static const char mainargs[MAINARGS_MAX_LEN] = TOSTRING(MAINARGS_PLACEHOLDER); // defined in CFLAGS
 
 void putch(char ch) {
-  outb(0x10000000, (uint8_t)ch);
+  outb(SERIAL_ADDR, (uint8_t)ch);
 }
 
 void halt(int code) {
