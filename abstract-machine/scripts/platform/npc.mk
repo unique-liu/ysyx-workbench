6c5ff1dddc8c11aa4d514fe426f303c18ee13686
tracer-ysyx <tracer@ysyx.org>
2026-04-10 16:08:09 +0800

    > sim RTL 2023k8009929022 刘遥青 Linux liu-virtual-machine 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:08:09 up  1:56,  0 users,  load average: 0.26, 0.62, 0.73

--- abstract-machine/scripts/platform/npc.mk
+++ abstract-machine/scripts/platform/npc.mk
@@ -26,6 +26,6 @@ image: image-dep
 	@$(OBJCOPY) -S --set-section-flags .bss=alloc,contents -O binary $(IMAGE).elf $(IMAGE).bin
 
 run: insert-arg
-	$(MAKE) -C $(NPC_HOME) sim IMG=$(IMAGE)
+	$(MAKE) -C $(NPC_HOME) sim IMG=$(IMAGE).bin
 
 .PHONY: insert-arg
