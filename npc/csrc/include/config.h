4f005a650ac9d546b334f60141aba431753fcb13
tracer-ysyx <tracer@ysyx.org>
2026-04-10 14:25:45 +0800

    > sim RTL 2023k8009929022 刘遥青 Linux liu-virtual-machine 6.8.0-40-generic #40~22.04.3-Ubuntu SMP PREEMPT_DYNAMIC Tue Jul 30 17:30:19 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:25:45 up 14 min,  0 users,  load average: 1.32, 1.34, 1.21

--- npc/csrc/include/config.h
+++ npc/csrc/include/config.h
@@ -3,12 +3,12 @@
 
 #define DEBUG // 开启调试模式，启用日志记录等功能
 #define CONFIG_DEVICE // 开启设备模拟功能
-#define CONFIG_ITRACE // 开启指令跟踪功能
-#define CONFIG_MTRACE // 开启内存访问日志记录功能
-#define CONFIG_FTRACE // 开启函数调用跟踪功能 
-#define CONFIG_DTRACE // 开启设备模拟功能
-#define CONFIG_RTRACE // 开启寄存器访问日志记录功能
-#define CONFIG_WATCHPOINT // 开启监视点功能
+// #define CONFIG_ITRACE // 开启指令跟踪功能
+// #define CONFIG_MTRACE // 开启内存访问日志记录功能
+// #define CONFIG_FTRACE // 开启函数调用跟踪功能 
+// #define CONFIG_DTRACE // 开启设备模拟功能
+// #define CONFIG_RTRACE // 开启寄存器访问日志记录功能
+// #define CONFIG_WATCHPOINT // 开启监视点功能
 
 
 #endif // CONFIG_H
