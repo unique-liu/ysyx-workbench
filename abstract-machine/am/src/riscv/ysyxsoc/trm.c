59a6f4d88581c795d6efe14e03c82664157d24b4
2023k8009929022-LiuYaoqing <liuyaoqing23@mails.ucas.ac.cn>
2026-06-02 23:25:24 +0800

    B2: set all the addresses

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -5,13 +5,15 @@
 #define SERIAL_ADDR 0x10000000L
 
 extern char _heap_start;
+extern char _text_ma_start,_text_ma_end,_text_sa_start,_text_sa_end;
+extern char _bootloader_ma_start,_bootloader_ma_end,_bootloader_sa_start,_bootloader_sa_end;
 extern char _data_ma_start,_data_ma_end,_data_sa_start,_data_sa_end;
 extern char _bss_sa_start,_bss_sa_end;
 
 int main(const char *args);
 
 extern char _pmem_start;
-#define PMEM_SIZE 0x00002000//sram
+#define PMEM_SIZE 0x400000//psram size
 #define PMEM_END  ((uintptr_t)&_heap_start + PMEM_SIZE)
 
 Area heap = RANGE(&_bss_sa_end, PMEM_END);// 堆区从 bss 段结束开始，到物理内存末尾
@@ -47,14 +49,39 @@ void print_myid(){
   printf("marchid: 0x%08x\n", marchid);
 }
 
+__attribute__((section(".entry")))
+void first_loader() {
+  char *src = &_bootloader_ma_start;
+  char *dst = &_bootloader_sa_start;
+  while (dst < &_bootloader_sa_end) {
+    *dst++ = *src++;
+  }
+}
+
+__attribute__((section(".bootloader")))
+void second_loader() {
+  char *src = &_text_ma_start;
+  char *dst = &_text_sa_start;
+  while (dst < &_text_sa_end) {
+    *dst++ = *src++;
+  }
+  src = &_data_ma_start;
+  dst = &_data_sa_start;
+  while (dst < &_data_sa_end) {
+    *dst++ = *src++;
+  }
+  char *bss = &_bss_sa_start;
+  while (bss < &_bss_sa_end) {
+    *bss++ = 0;
+  }
+}
+
+// __attribute__((section(".bootloader")))
 void _trm_init() {
-  //load data and set bss
-  memcpy(&_data_sa_start, &_data_ma_start, &_data_ma_end - &_data_ma_start);
-  memset(&_bss_sa_start, 0, &_bss_sa_end - &_bss_sa_start);
   //init uart
   init_uart();
   //print mvendorid and marchid 
-  // print_myid();
+  print_myid();
   // print_myid();
 
   
