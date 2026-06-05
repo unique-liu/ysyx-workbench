8786079d9c2c612efa063ba017f3a1f5984b1925
2023k8009929022-LiuYaoqing <liuyaoqing23@mails.ucas.ac.cn>
2026-06-05 22:09:23 +0800

    B2: support RT-Thread in soc: load its special sections and print load info

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -3,12 +3,16 @@
 #include <klib.h>
 #include "../riscv.h"
 #define SERIAL_ADDR 0x10000000L
-
+#define __RTTHREAD__
 extern char _heap_start;
 extern char _text_ma_start,_text_ma_end,_text_sa_start,_text_sa_end;
 extern char _bootloader_ma_start,_bootloader_ma_end,_bootloader_sa_start,_bootloader_sa_end;
 extern char _data_ma_start,_data_ma_end,_data_sa_start,_data_sa_end;
 extern char _bss_sa_start,_bss_sa_end;
+#ifdef __RTTHREAD__
+extern char _edata_ma_start,_edata_ma_end,_edata_sa_start,_edata_sa_end;
+extern char _ebss_sa_start,_ebss_sa_end;
+#endif
 
 int main(const char *args);
 
@@ -20,7 +24,7 @@ Area heap = RANGE(&_bss_sa_end, PMEM_END);// 堆区从 bss 段结束开始，到
 static const char mainargs[MAINARGS_MAX_LEN] = TOSTRING(MAINARGS_PLACEHOLDER); // defined in CFLAGS
 
 
-
+__attribute__((section(".entry")))
 void init_uart(){
   // 初始化串口，设置波特率等
   // outb(SERIAL_ADDR + 1, 0x00); // 禁止中断
@@ -29,6 +33,10 @@ void init_uart(){
   outb(SERIAL_ADDR + 1, 0x00); // 波特率分频器高字节
   outb(SERIAL_ADDR + 3, 0x03); // 设置数据位为8，停止位为1，无奇偶校验，并且禁用波特率分频器访问
 }
+#define putch_inline(ch) do { \
+  while((inb(SERIAL_ADDR+5)&0x20) == 0); /* 等待发送缓冲区空 */ \
+  outb(SERIAL_ADDR, (uint8_t)(ch)); \
+} while(0)
 
 void putch(char ch) {
   while((inb(SERIAL_ADDR+5)&0x20) == 0); // 等待发送缓冲区空
@@ -49,41 +57,73 @@ void print_myid(){
   printf("marchid: 0x%08x\n", marchid);
 }
 
+void print_loadinfo(){
+  printf("text:0x%08x-0x%08x -> 0x%08x-0x%08x\n", &_text_ma_start, &_text_ma_end, &_text_sa_start, &_text_sa_end);
+  printf("bootloader:0x%08x-0x%08x -> 0x%08x-0x%08x\n", &_bootloader_ma_start, &_bootloader_ma_end, &_bootloader_sa_start, &_bootloader_sa_end);
+  printf("data:0x%08x-0x%08x -> 0x%08x-0x%08x\n", &_data_ma_start, &_data_ma_end, &_data_sa_start, &_data_sa_end);
+  printf("bss: void -> 0x%08x-0x%08x\n", &_bss_sa_start, &_bss_sa_end);
+  #ifdef __RTTHREAD__
+  printf("edata:0x%08x-0x%08x -> 0x%08x-0x%08x\n", &_edata_ma_start, &_edata_ma_end, &_edata_sa_start, &_edata_sa_end);
+  printf("ebss: void -> 0x%08x-0x%08x\n", &_ebss_sa_start, &_ebss_sa_end);
+  #endif
+}
+
 __attribute__((section(".entry")))
 void first_loader() {
+  init_uart();
+  putch_inline('1');
   char *src = &_bootloader_ma_start;
   char *dst = &_bootloader_sa_start;
   while (dst < &_bootloader_sa_end) {
     *dst++ = *src++;
   }
+  putch_inline('1');
 }
 
 __attribute__((section(".bootloader")))
 void second_loader() {
+  putch_inline('2');
   char *src = &_text_ma_start;
   char *dst = &_text_sa_start;
+  putch_inline('t');
   while (dst < &_text_sa_end) {
     *dst++ = *src++;
   }
   src = &_data_ma_start;
   dst = &_data_sa_start;
+  putch_inline('d');
   while (dst < &_data_sa_end) {
     *dst++ = *src++;
   }
   char *bss = &_bss_sa_start;
+  putch_inline('b');
   while (bss < &_bss_sa_end) {
     *bss++ = 0;
   }
+
+  #ifdef __RTTHREAD__
+  src = &_edata_ma_start;
+  dst = &_edata_sa_start;
+  putch('D');
+  while (dst < &_edata_sa_end) {
+    *dst++ = *src++;
+  }
+  bss = &_ebss_sa_start;
+  putch('B');
+  while (bss < &_ebss_sa_end) {
+    *bss++ = 0;
+  }
+  #endif
+  putch_inline('2');
 }
 
 // __attribute__((section(".bootloader")))
 void _trm_init() {
-  //init uart
-  init_uart();
+  putch('\n');
   //print mvendorid and marchid 
   print_myid();
-  // print_myid();
-
+  // print load info
+  print_loadinfo();
   
   int ret = main(mainargs);
   halt(ret);
